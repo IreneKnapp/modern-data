b19936bc7275cf579243f57c6afdaeb16de57920
Irene Knapp <irene.knapp@icloud.com>
2012-01-20 23:33:59 +0000

    Progress.

--- Data/Modern.hs
+++ Data/Modern.hs
@@ -1,20 +1,24 @@
 module Data.Modern
   (ModernType(..),
    ModernData(..),
-   ModernCommandType(..),
-   ModernCommand(..),
-   ModernHash(..),
-   ModernBitpath(..),
-   ModernTypeName(..),
-   ModernFieldName(..),
-   ModernContext(..))
+   ModernHash,
+   ModernBitpath,
+   ModernTypeName,
+   ModernFieldName,
+   ModernContext,
+   initialContext,
+   computeTypeHash)
   where
 
+import Control.Monad.State
 import Data.Array
+import Data.Bits
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.UTF8 as UTF8
+import Data.Digest.Murmur3
 import Data.Int
+import Data.List
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.String
@@ -22,21 +26,21 @@ import Data.Word
 
 
 data ModernType
-  = ModernTypeInt8
-  | ModernTypeInt16
-  | ModernTypeInt32
-  | ModernTypeInt64
-  | ModernTypeWord8
-  | ModernTypeWord16
-  | ModernTypeWord32
-  | ModernTypeWord64
-  | ModernTypeFloat
-  | ModernTypeDouble
-  | ModernTypeUTF8
-  | ModernTypeBlob
-  | ModernTypeList ModernType
-  | ModernTypeUnion ModernTypeName [(ModernBitpath, ModernHash)]
-  | ModernTypeStructure ModernTypeName [(ModernFieldName, ModernHash)]
+  = ModernInt8Type
+  | ModernInt16Type
+  | ModernInt32Type
+  | ModernInt64Type
+  | ModernWord8Type
+  | ModernWord16Type
+  | ModernWord32Type
+  | ModernWord64Type
+  | ModernFloatType
+  | ModernDoubleType
+  | ModernUTF8Type
+  | ModernBlobType
+  | ModernListType ModernType
+  | ModernUnionType ModernTypeName [(ModernBitpath, ModernType)]
+  | ModernStructureType ModernTypeName [(ModernFieldName, ModernType)]
 
 
 data ModernData
@@ -62,24 +66,18 @@ data ModernCommandType
   | ModernCommandTypeInquire
   | ModernCommandTypeAttach
   | ModernCommandTypeDetach
+  | ModernCommandTypeBeginConfiguration
+  | ModernCommandTypeEndConfiguration
   | ModernCommandTypeUnionType
   | ModernCommandTypeStructureType
-  | ModernCommandTypeDataInt8
-  | ModernCommandTypeDataInt16
-  | ModernCommandTypeDataInt32
-  | ModernCommandTypeDataInt64
-  | ModernCommandTypeDataWord8
-  | ModernCommandTypeDataWord16
-  | ModernCommandTypeDataWord32
-  | ModernCommandTypeDataWord64
-  | ModernCommandTypeDataFloat
-  | ModernCommandTypeDataDouble
-  | ModernCommandTypeDataUTF8
-  | ModernCommandTypeDataShortBlob
-  | ModernCommandTypeDataLongBlob
-  | ModernCommandTypeDataShortList
-  | ModernCommandTypeDataLongList
-  | ModernCommandTypeDataAttachedType ModernBitpath
+  | ModernCommandTypeAtom
+  | ModernCommandTypeUTF8
+  | ModernCommandTypeShortBlob
+  | ModernCommandTypeLongBlob
+  | ModernCommandTypeShortList ModernType
+  | ModernCommandTypeLongList ModernType
+  | ModernCommandTypeUnion (ModernAttachments ModernType)
+  | ModernCommandTypeStructure ModernType
 
 
 data ModernCommand
@@ -87,9 +85,13 @@ data ModernCommand
   | ModernCommandInquire ModernHash
   | ModernCommandAttach ModernBitpath ModernHash
   | ModernCommandDetach ModernBitpath
-  | ModernCommandUnionType ModernTypeName [(ModernBitpath, ModernHash)]
-  | ModernCommandStructureType ModernTypeName [(ModernFieldName, ModernHash)]
-  | ModernCommandData ModernType ModernData
+  | ModernCommandBeginConfiguration ModernTypeName
+  | ModernCommandEndConfiguration ModernTypeName
+  | ModernCommandUnionType ModernTypeName
+                           (ModernAttachments ModernHash)
+  | ModernCommandStructureType ModernTypeName
+                               [(ModernFieldName, ModernHash)]
+  | ModernCommandData ModernData
 
 
 data ModernHash
@@ -98,7 +100,22 @@ data ModernHash
 
 
 data ModernBitpath
-  = ModernBitpath Int Word64
+  = ModernBitpath Word8 Word64
+
+
+data Bit = Zero | One
+
+
+bitpathToList :: ModernBitpath -> [Bit]
+bitpathToList (ModernBitpath count source) =
+  unfoldr (\i ->
+             if fromIntegral i == count
+               then Nothing
+               else Just (case fromIntegral $ shiftR source i .&. 1 of
+                            0 -> Zero
+                            _ -> One,
+                          i + 1))
+          0
 
 
 data ModernTypeName
@@ -118,13 +135,81 @@ instance IsString ModernFieldName where
 data ModernContext =
   ModernContext {
       modernContextTypes :: Map ModernHash ModernType,
-      modernContextAttachments :: ModernContextAttachments
+      modernContextCommands :: ModernAttachments ModernCommandType
     }
 
 
-data ModernContextAttachments
-  = Attached ModernCommandType
+data ModernAttachments client
+  = Attached client
+  | Unattached
   | Split {
-        splitZero :: ModernContextAttachments,
-        splitOne :: ModernContextAttachments
+        splitZero :: ModernAttachments client,
+        splitOne :: ModernAttachments client
       }
+
+
+initialContext :: ModernContext
+initialContext =
+  ModernContext {
+      modernContextTypes = undefined,
+      modernContextCommands = undefined
+    }
+
+
+computeTypeHash :: ModernType -> ModernHash
+computeTypeHash modernType =
+  ModernHash $ asByteString $ hash $ computeTypeByteString modernType
+
+
+computeTypeByteString :: ModernType -> ByteString
+computeTypeByteString modernType =
+  let typeHelper modernType =
+        case modernType of
+          ModernInt8Type -> UTF8.fromString "Int8"
+          ModernInt16Type -> UTF8.fromString "Int16"
+          ModernInt32Type -> UTF8.fromString "Int32"
+          ModernInt64Type -> UTF8.fromString "Int64"
+          ModernWord8Type -> UTF8.fromString "Word8"
+          ModernWord16Type -> UTF8.fromString "Word16"
+          ModernWord32Type -> UTF8.fromString "Word32"
+          ModernWord64Type -> UTF8.fromString "Word64"
+          ModernFloatType -> UTF8.fromString "Float"
+          ModernDoubleType -> UTF8.fromString "Double"
+          ModernUTF8Type -> UTF8.fromString "UTF8"
+          ModernBlobType -> UTF8.fromString "Blob"
+          ModernListType contentType ->
+            BS.concat [UTF8.fromString "List(",
+                       typeHelper contentType,
+                       UTF8.fromString ")"]
+          ModernUnionType (ModernTypeName name) possibilities ->
+            BS.concat [UTF8.fromString "Union(",
+                       name,
+                       BS.pack [0x00],
+                       BS.intercalate
+                        (UTF8.fromString ",")
+                        $ map (\(bitpath, possibilityType) ->
+                                  BS.concat [bitpathHelper bitpath,
+                                             typeHelper possibilityType])
+                              possibilities,
+                       UTF8.fromString ")"]
+          ModernStructureType (ModernTypeName name) fields ->
+            BS.concat [UTF8.fromString "Structure(",
+                       name,
+                       BS.pack [0x00],
+                       BS.intercalate
+                        (UTF8.fromString ",")
+                        $ map (\(ModernFieldName fieldName, fieldType) ->
+                                  BS.concat [fieldName,
+                                             BS.pack [0x00],
+                                             typeHelper fieldType])
+                              fields,
+                       UTF8.fromString ")"]
+      bitpathHelper bitpath =
+        UTF8.fromString
+         $ map (\bit -> case bit of
+                  Zero -> '0'
+                  One -> '1')
+               $ bitpathToList bitpath
+  in BS.concat [UTF8.fromString "Type(",
+                typeHelper modernType,
+                UTF8.fromString ")"]
