66e05d39df78ad9b780e8845d202fba194c9ffbd
Irene Knapp <irene.knapp@icloud.com>
2012-05-08 19:52:49 +0000

    documentSchema changes (more of them).

--- Data/Modern/Documentation.hs
+++ Data/Modern/Documentation.hs
@@ -1,9 +1,8 @@
 module Data.Modern.Documentation
-  (documentSchema,
-   quoteString,
-   escapeString)
+  (documentSchema)
   where
 
+import qualified Data.Array as Array
 import qualified Data.ByteString.UTF8 as UTF8
 import Data.Function
 import Data.List
@@ -18,12 +17,12 @@ import Data.Modern.Initial
 import Data.Modern.Types
 
 
-data Node =
-  Node {
-      nodeNames :: [String],
-      nodeType :: Maybe NodeType,
-      nodeBody :: Maybe [Node]
-    }
+data Node = Node [String] [NodePart]
+
+
+data NodePart
+  = SymbolNodePart String
+  | BlockNodePart [Node]
 
 
 data NodeType
@@ -67,7 +66,7 @@ documentSchema context =
 	       (Map.empty, Set.empty)
 	       (Map.toList $ modernContextTypes context)
       interestingNodes =
-	sortBy (on compare nodeNames)
+	sortBy (on compare (\(Node names _) -> names))
 	       (Map.foldlWithKey (\interestingNodesSoFar typeHash node ->
 			            if Set.member typeHash
 					          allRedundantTypeHashes
@@ -75,101 +74,59 @@ documentSchema context =
 				      else node : interestingNodesSoFar)
 			         []
 				 allNodes)
-      primitiveNode theNodeType =
-	Node {
-	    nodeNames = [],
-	    nodeType = Just theNodeType,
-	    nodeBody = Nothing
-	  }
-      nodeFor ModernInt8Type = primitiveNode Int8NodeType
-      nodeFor ModernInt16Type = primitiveNode Int16NodeType
-      nodeFor ModernInt32Type = primitiveNode Int32NodeType
-      nodeFor ModernInt64Type = primitiveNode Int64NodeType
-      nodeFor ModernNat8Type = primitiveNode Nat8NodeType
-      nodeFor ModernNat16Type = primitiveNode Nat16NodeType
-      nodeFor ModernNat32Type = primitiveNode Nat32NodeType
-      nodeFor ModernNat64Type = primitiveNode Nat64NodeType
-      nodeFor ModernFloat32Type = primitiveNode Float32NodeType
-      nodeFor ModernFloat64Type = primitiveNode Float64NodeType
-      nodeFor ModernUTF8Type = primitiveNode UTF8NodeType
-      nodeFor ModernBlobType = primitiveNode BlobNodeType
+      nodeFor ModernInt8Type = Node [] [SymbolNodePart "int8"]
+      nodeFor ModernInt16Type = Node [] [SymbolNodePart "int16"]
+      nodeFor ModernInt32Type = Node [] [SymbolNodePart "int32"]
+      nodeFor ModernInt64Type = Node [] [SymbolNodePart "int64"]
+      nodeFor ModernNat8Type = Node [] [SymbolNodePart "nat8"]
+      nodeFor ModernNat16Type = Node [] [SymbolNodePart "nat16"]
+      nodeFor ModernNat32Type = Node [] [SymbolNodePart "nat32"]
+      nodeFor ModernNat64Type = Node [] [SymbolNodePart "nat64"]
+      nodeFor ModernFloat32Type = Node [] [SymbolNodePart "float32"]
+      nodeFor ModernFloat64Type = Node [] [SymbolNodePart "float64"]
+      nodeFor ModernUTF8Type = Node [] [SymbolNodePart "utf8"]
+      nodeFor ModernBlobType = Node [] [SymbolNodePart "blob"]
       nodeFor (ModernListType contentType) =
-	Node {
-	    nodeNames = [],
-	    nodeType = Just ListNodeType,
-	    nodeBody = Nothing
-	  }
-      nodeFor (ModernTupleType contentTypes) =
-        -- (Maybe (Array Nat64 ModernType))
-	Node {
-	    nodeNames = [],
-	    nodeType = Just TupleNodeType,
-	    nodeBody = Nothing
-	  }
-      nodeFor (ModernUnionType possibilities) =
-        -- (Maybe (Nat8, (Array Nat64 ModernType)))
-        Node {
-            nodeNames = [],
-	    nodeType = Just UnionNodeType,
-	    nodeBody = Nothing
-          }
+	let (Node _ childNodeParts) = childNodeFor contentType
+	in Node [] (SymbolNodePart "list" : childNodeParts)
+      nodeFor (ModernTupleType maybeContentTypes) =
+	let contentTypes = maybe [] Array.elems maybeContentTypes
+	in Node []
+		[SymbolNodePart "tuple",
+                 BlockNodePart $ map childNodeFor contentTypes]
+      nodeFor (ModernUnionType maybePossibilities) =
+	let possibilities = maybe [] (Array.elems . snd) maybePossibilities
+	in Node []
+		[SymbolNodePart "union",
+		 BlockNodePart $ map childNodeFor possibilities]
       nodeFor (ModernStructureType fieldTypes) =
         -- (Maybe (Array Nat64 (ModernFieldName, ModernType)))
-        Node {
-            nodeNames = [],
-	    nodeType = Just StructureNodeType,
-            nodeBody = Nothing
-          }
+	Node []
+	     [SymbolNodePart "structure",
+	      BlockNodePart []]
       nodeFor (ModernNamedType (ModernTypeName name) contentType) =
-	let childNode = nodeFor contentType
-	in childNode {
-	       nodeNames = UTF8.toString name : nodeNames childNode
-	     }
-      outputNodes indentation nodes =
-	intercalate (outputIndentation indentation ++ "\n")
-		    (map (outputNode indentation) nodes)
-      outputNode indentation node =
-	let headerItems =
-	      (map quoteString $ nodeNames node)
-	      ++ (case nodeType node of
-		    Nothing -> []
-                    Just Int8NodeType -> ["int8"]
-                    Just Int16NodeType -> ["int16"]
-                    Just Int32NodeType -> ["int32"]
-                    Just Int64NodeType -> ["int64"]
-                    Just Nat8NodeType -> ["nat8"]
-                    Just Nat16NodeType -> ["nat16"]
-                    Just Nat32NodeType -> ["nat32"]
-                    Just Nat64NodeType -> ["nat64"]
-                    Just Float32NodeType -> ["float32"]
-                    Just Float64NodeType -> ["float64"]
-                    Just UTF8NodeType -> ["utf8"]
-                    Just BlobNodeType -> ["blob"]
-                    Just ListNodeType -> ["list"]
-                    Just TupleNodeType -> ["tuple"]
-                    Just UnionNodeType -> ["union"]
-                    Just StructureNodeType -> ["structure"])
-	    header = intercalate " " headerItems
-	 in case nodeBody node of
-	      Nothing ->
-		outputIndentation indentation ++ header ++ ";\n"
-	      Just children ->
-	        outputIndentation indentation ++ header ++ " {\n"
-	        ++ outputNodes (indentation + 1) children
-	        ++ outputIndentation indentation ++ "}\n"
-      outputIndentation indentation =
-	take (indentation * 4) (repeat ' ')
-  in outputNodes 0 interestingNodes
-
-
-quoteString :: String -> String
-quoteString unquoted = "'" ++ escapeString unquoted ++ "'"
-
-
-escapeString :: String -> String
-escapeString unescaped =
-  concat $ map (\c -> if c == '\''
-                        then "''"
-			else [c])
-               unescaped
+	let (Node childNames childNodeParts) = nodeFor contentType
+	in Node (UTF8.toString name : childNames)
+		(childNodeParts ++ [SymbolNodePart $ UTF8.toString name])
+      childNodeFor (ModernNamedType (ModernTypeName name) contentType) =
+	Node [] [SymbolNodePart $ UTF8.toString name]
+      childNodeFor otherType = nodeFor otherType
+      buildLine words = intercalate " " words
+      linesForNode (Node _ parts) =
+	let (lines, words) =
+	      foldl' (\(lines, words) part ->
+			 case part of
+			   SymbolNodePart symbol ->
+			     (lines, words ++ [symbol])
+			   BlockNodePart subnodes ->
+			     (lines
+			      ++ [buildLine $ words ++ ["{"]]
+			      ++ (map (\line -> "    " ++ line)
+				      (concat $ map linesForNode subnodes)),
+			      ["}"]))
+		     ([], [])
+		     parts
+        in lines ++ [buildLine words ++ ";"]
+  in concat $ map (\line -> line ++ "\n")
+		  $ intercalate [""] $ map linesForNode interestingNodes
 
