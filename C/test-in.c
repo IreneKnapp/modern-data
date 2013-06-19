804a8cbc22dcbcc8f4dc2304bab732935ad69739
Irene Knapp <irene.knapp@icloud.com>
2013-06-19 19:49:51 +0000

    The test test-in sorta runs now.

--- C/test-in.c
+++ C/test-in.c
@@ -48,38 +48,56 @@ static int test_explicatory_file_input
         (struct test_context *) test_context_in;
     test_suite *test_suite = test_context->test_suite;
     modern_library *library = test_context->library;    
+    struct modern_processor *processor = test_context->processor;
     
-    allow_allocation(test_suite);
-    void *process_state = processor->initialize(library);
-    disallow_allocation(test_suite);
+    int succeeded = 1;
     
-    struct modern_stream *stream = malloc(sizeof(struct modern_stream));
-    struct stream_state *stream_state = malloc(sizeof(struct stream_state));
-    
-    FILE *file = fopen("input.modern-explicatory", "r");
-    if(!file)
-    {
-        return 0;
+    void *process_state = NULL;
+    if(succeeded) {
+        allow_allocation(test_suite);
+        process_state = processor->initialize(library);
+        if(!process_state) succeeded = 0;
+        disallow_allocation(test_suite);
     }
     
-    allow_allocation(test_suite);
-    struct modern_vfile *vfile = modern_vfile_stdio_make(library);
-    disallow_allocation(test_suite);
+    struct modern_stream *stream = NULL;
+    if(succeeded) {
+        stream = malloc(sizeof(struct modern_stream));
+        if(!stream) succeeded = 0;
+    }
     
-    allow_allocation(test_suite);
-    void *vfile_state = modern_vfile_stdio_initialize(library, file);
-    disallow_allocation(test_suite);
+    struct stream_state *stream_state = NULL;
+    if(succeeded) {
+        stream_state = malloc(sizeof(struct stream_state));
+        if(!stream_state) succeeded = 0;
+    }
     
-    processor->run(process_state, stream, stream_state, vfile, vfile_state);
+    FILE *file = NULL;
+    if(succeeded) {
+        fopen("input.modern-explicatory", "r");
+        if(!file) succeeded = 0;
+    }
     
-    allow_deallocation(test_suite);
-    modern_vfile_stdio_finalize(library, vfile);
-    disallow_deallocation(test_suite);
+    if(succeeded) {
+        allow_allocation(test_suite);
+        struct modern_vfile *vfile = modern_vfile_stdio_make(library);
+        disallow_allocation(test_suite);
+        
+        allow_allocation(test_suite);
+        void *vfile_state = modern_vfile_stdio_initialize(library, file);
+        disallow_allocation(test_suite);
+        
+        processor->run(process_state, stream, stream_state, vfile, vfile_state);
+        
+        allow_deallocation(test_suite);
+        modern_vfile_stdio_finalize(library, vfile);
+        disallow_deallocation(test_suite);
+    }
     
-    fclose(file);
+    if(file) fclose(file);
     
-    free(stream_state);
-    free(stream);
+    if(stream_state) free(stream_state);
+    if(stream) free(stream);
     
     allow_deallocation(test_suite);
     processor->finalize(process_state);
