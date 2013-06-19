76d4480bf397b0e385c45755ec5cc6648a4ccf21
Irene Knapp <irene.knapp@icloud.com>
2013-06-19 21:45:10 +0000

    Progress on processor-explicatory.c; its basic control flow is in place now, though no parsing or anything.

--- C/processor-explicatory.c
+++ C/processor-explicatory.c
@@ -3,12 +3,18 @@
 
 
 struct processor_explicatory_state {
+    struct modern_process process;
     struct modern_library *library;
+    int started : 1;
+    int ended : 1;
+    int aborted : 1;
 };
 
 
 HELPER void *processor_explicatory_initialize(modern_library *library);
 HELPER void processor_explicatory_finalize(void *process_state);
+HELPER void processor_explicatory_abort(void *process_state);
+HELPER void processor_explicatory_flush(void *process_state);
 HELPER void processor_explicatory_step
   (void *process_state,
    struct modern_stream *stream, void *stream_state,
@@ -53,8 +59,13 @@ HELPER void *processor_explicatory_initialize
         library->error_handler->memory(library->client_state, size);
         return NULL;
     }
-
+    
+    state->process.abort = processor_explicatory_abort;
+    state->process.flush = processor_explicatory_flush;
     state->library = library;
+    state->started = 0;
+    state->ended = 0;
+    state->aborted = 0;
     
     return state;
 }
@@ -70,18 +81,50 @@ HELPER void processor_explicatory_finalize(void *process_state_in)
 }
 
 
-HELPER void processor_explicatory_step
-  (void *process_state,
-   struct modern_stream *stream, void *stream_state,
-   struct modern_vfile *vfile, void *vfile_state)
+HELPER void processor_explicatory_abort(void *process_state)
 {
 }
 
 
-HELPER void processor_explicatory_run
-  (void *process_state,
+HELPER void processor_explicatory_flush(void *process_state)
+{
+}
+
+
+HELPER void processor_explicatory_step
+  (void *process_state_in,
    struct modern_stream *stream, void *stream_state,
    struct modern_vfile *vfile, void *vfile_state)
 {
+    struct processor_explicatory_state *process_state =
+        (struct processor_explicatory_state *) process_state_in;
+    struct modern_library *library = process_state->library;
+    
+    process_state->aborted = 0;
+    
+    if(!process_state->started) {
+        process_state->started = 1;
+        stream->start(&process_state->process, process_state_in, stream_state);
+    } else if(!process_state->ended) {
+        process_state->ended = 1;
+    }
+}
+
+
+HELPER void processor_explicatory_run
+  (void *process_state_in,
+   struct modern_stream *stream, void *stream_state,
+   struct modern_vfile *vfile, void *vfile_state)
+{
+    struct processor_explicatory_state *process_state =
+        (struct processor_explicatory_state *) process_state_in;
+    
+    process_state->aborted = 0;
+    
+    while(!process_state->ended && !process_state->aborted)
+    {
+        processor_explicatory_step
+            (process_state_in, stream, stream_state, vfile, vfile_state);
+    }
 }
 
