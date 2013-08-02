3b8b1a7f4e555e7e63fd32cc36598f858ef7c785
Irene Knapp <irene.knapp@icloud.com>
2013-08-02 20:40:06 +0000

    Lens stuff.  Heh.

--- Tools/Build/Haskell/Main.hs
+++ Tools/Build/Haskell/Main.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE ExistentialQuantification, OverloadedStrings,
-             DeriveDataTypeable #-}
+             DeriveDataTypeable, TemplateHaskell #-}
 module Main (main) where
 
 import qualified Data.Set as Set
 import qualified Data.Text as Text
 import qualified System.Directory as IO
 
+import Control.Lens
 import Control.Monad
 import Data.Function
 import Data.List
@@ -17,8 +18,232 @@ import Data.Typeable
 class TextShow textShow where
   textShow :: textShow -> Text.Text
 
-class Named named where
-  namedName :: named -> Text.Text
+class HasName hasName where
+  name :: Simple Lens hasName Text.Text
+class HasLanguage hasLanguage where
+  language :: Simple Lens hasLanguage Language
+
+data Language
+  = CLanguage
+  | HaskellLanguage
+  deriving (Eq, Ord)
+instance TextShow Language where
+  textShow CLanguage = "c-language"
+  textShow HaskellLanguage = "haskell-language"
+
+data Provenance
+  = InputProvenance
+  | BuiltProvenance
+  | SystemProvenance
+  deriving (Eq, Ord)
+instance TextShow Provenance where
+  textShow InputProvenance = "input-provenance"
+
+data FileType
+  = UnknownFileType
+  | HeaderFileType Language
+  | SourceFileType Language
+  | ObjectFileType
+  | ExecutableFileType
+  | LibraryFileType
+  deriving (Eq, Ord)
+
+class (TextShow file, Eq file, Ord file, Typeable file) => File file where
+  fromAnyFile :: AnyFile -> Maybe file
+  fileType :: Simple Lens file FileType
+  path :: Simple Lens file Text.Text
+  provenance :: Simple Lens file Provenance
+
+data AnyFile = forall file . File file => AnyFile file
+  deriving (Typeable)
+makeLenses ''AnyFile
+instance Eq AnyFile where
+  (==) a b =
+    case a of
+      AnyFile a' -> case fromAnyFile b of
+                      Just b' -> (==) a' b'
+                      Nothing -> False
+instance Ord AnyFile where
+  compare a b =
+    case a of
+      AnyFile a' -> case fromAnyFile b of
+                      Just b' -> compare a' b'
+                      Nothing -> on compare (view fileType) a b
+instance File AnyFile where
+  fromAnyFile (AnyFile file) = cast file
+  fileType (AnyFile file) = fileType . anyFile
+  path (AnyFile file) = path . anyFile
+  provenance (AnyFile file) = provenance . anyFile
+instance TextShow AnyFile where
+  textShow (AnyFile file) = textShow file
+
+data Task
+  = AmalgamationTask
+  | BinaryTask
+  | TestTask
+  | DebugTask
+  | CleanTask
+  deriving (Eq, Ord)
+
+data HeaderFile =
+  HeaderFile {
+      _headerFileLanguage :: Language,
+      _headerFilePath :: Text.Text,
+      _headerFileProvenance :: Provenance
+    }
+  deriving (Typeable)
+makeLenses ''HeaderFile
+instance Eq HeaderFile where
+  (==) a b =
+    foldl1 (&&)
+           [on (==) (view language) a b,
+            on (==) (view path) a b,
+            on (==) (view provenance) a b]
+instance Ord HeaderFile where
+  compare a b =
+    mconcat [on compare (view language) a b,
+             on compare (view path) a b,
+             on compare (view provenance) a b]
+instance File HeaderFile where
+  fromAnyFile (AnyFile file) = cast file
+  fileType file = HeaderFileType $ file^.language
+  filePath = view path
+  fileProvenance = view provenance
+instance TextShow HeaderFile where
+  textShow file =
+    Text.concat $
+      ["(header-file \"",
+       file^.path,
+       "\" ",
+       textShow $ file^.provenance,
+       " ",
+       textShow $ file^.language,
+       ")"]
+
+data SourceFile =
+  SourceFile {
+      _sourceFileLanguage :: Language,
+      _sourceFilePath :: Text.Text,
+      _sourceFileProvenance :: Provenance
+    }
+  deriving (Typeable)
+makeLenses ''SourceFile
+instance Eq SourceFile where
+  (==) a b =
+    foldl1 (&&)
+           [on (==) sourceFileLanguage a b,
+            on (==) sourceFilePath a b,
+            on (==) sourceFileProvenance a b]
+instance Ord SourceFile where
+  compare a b =
+    mconcat [on compare sourceFileLanguage a b,
+             on compare sourceFilePath a b,
+             on compare sourceFileProvenance a b]
+instance File SourceFile where
+  fromAnyFile (AnyFile file) = cast file
+  fileType file = SourceFileType $ sourceFileLanguage file
+  filePath = sourceFilePath
+  fileProvenance = sourceFileProvenance
+instance TextShow SourceFile where
+  textShow file =
+    Text.concat $
+      ["(source-file \"",
+       sourceFilePath file,
+       "\" ",
+       textShow $ sourceFileProvenance file,
+       " ",
+       textShow $ sourceFileLanguage file,
+       ")"]
+
+data ObjectFile =
+  ObjectFile {
+      _objectFilePath :: Text.Text,
+      _objectFileProvenance :: Provenance
+    }
+  deriving (Typeable)
+makeLenses ''ObjectFile
+instance Eq ObjectFile where
+  (==) a b =
+    foldl1 (&&)
+           [on (==) objectFilePath a b,
+            on (==) objectFileProvenance a b]
+instance Ord ObjectFile where
+  compare a b =
+    mconcat [on compare objectFilePath a b,
+             on compare objectFileProvenance a b]
+instance File ObjectFile where
+  fromAnyFile (AnyFile file) = cast file
+  fileType _ = ObjectFileType
+  filePath = objectFilePath
+  fileProvenance = objectFileProvenance
+instance TextShow ObjectFile where
+  textShow file =
+    Text.concat $
+      ["(object-file \"",
+       objectFilePath file,
+       "\" ",
+       textShow $ objectFileProvenance file,
+       ")"]
+
+data ExecutableFile =
+  ExecutableFile {
+      _executableFilePath :: Text.Text,
+      _executableFileProvenance :: Provenance
+    }
+  deriving (Typeable)
+makeLenses ''ExecutableFile
+instance Eq ExecutableFile where
+  (==) a b =
+    foldl1 (&&)
+           [on (==) executableFilePath a b,
+            on (==) executableFileProvenance a b]
+instance Ord ExecutableFile where
+  compare a b =
+    mconcat [on compare executableFilePath a b,
+             on compare executableFileProvenance a b]
+instance File ExecutableFile where
+  fromAnyFile (AnyFile file) = cast file
+  fileType _ = ExecutableFileType
+  filePath = executableFilePath
+  fileProvenance = executableFileProvenance
+instance TextShow ExecutableFile where
+  textShow file =
+    Text.concat $
+      ["(executable-file \"",
+       executableFilePath file,
+       "\" ",
+       textShow $ executableFileProvenance file,
+       ")"]
+
+data LibraryFile =
+  LibraryFile {
+      _libraryFilePath :: Text.Text,
+      _libraryFileProvenance :: Provenance
+    }
+  deriving (Typeable)
+makeLenses ''LibraryFile
+instance Eq LibraryFile where
+  (==) a b =
+    foldl1 (&&)
+           [on (==) libraryFilePath a b,
+            on (==) libraryFileProvenance a b]
+instance Ord LibraryFile where
+  compare a b =
+    mconcat [on compare libraryFilePath a b,
+             on compare libraryFileProvenance a b]
+instance File LibraryFile where
+  fromAnyFile (AnyFile file) = cast file
+  fileType _ = LibraryFileType
+  filePath = libraryFilePath
+  fileProvenance = libraryFileProvenance
+instance TextShow LibraryFile where
+  textShow file =
+    Text.concat $
+      ["(library-file \"",
+       libraryFilePath file,
+       "\" ",
+       textShow $ libraryFileProvenance file,
+       ")"]
 
 class (TextShow buildStep) => BuildStep buildStep where
   buildStepInputs :: buildStep -> Set.Set AnyFile
@@ -36,18 +261,19 @@ instance BuildStep AnyBuildStep where
 instance TextShow AnyBuildStep where
   textShow (AnyBuildStep buildStep) = textShow buildStep
 
-class (Named target, TextShow target) => Target target where
+class (HasName target, TextShow target) => Target target where
   targetBuildSteps :: Task -> target -> [AnyBuildStep]
   targetPrerequisites :: target -> Set.Set AnyTarget
   targetProducts :: target -> Set.Set AnyFile
 
 data AnyTarget = forall target . Target target => AnyTarget target
+makeLenses ''AnyTarget
 instance Eq AnyTarget where
-  (==) a b = on (==) namedName a b
+  (==) a b = on (==) (view name) a b
 instance Ord AnyTarget where
-  compare a b = on compare namedName a b
-instance Named AnyTarget where
-  namedName (AnyTarget target) = namedName target
+  compare a b = on compare (view name) a b
+instance HasName AnyTarget where
+  name (AnyTarget target) = name . target
 instance Target AnyTarget where
   targetBuildSteps task (AnyTarget target) = targetBuildSteps task target
   targetPrerequisites (AnyTarget target) = targetPrerequisites target
@@ -55,78 +281,16 @@ instance Target AnyTarget where
 instance TextShow AnyTarget where
   textShow (AnyTarget target) = textShow target
 
-data FileType
-  = UnknownFileType
-  | HeaderFileType Language
-  | SourceFileType Language
-  | ObjectFileType
-  | ExecutableFileType
-  | LibraryFileType
-  deriving (Eq, Ord)
-
-class (TextShow file, Eq file, Ord file, Typeable file) => File file where
-  fromAnyFile :: AnyFile -> Maybe file
-  fileType :: file -> FileType
-  filePath :: file -> Text.Text
-  fileProvenance :: file -> Provenance
-
-data AnyFile = forall file . File file => AnyFile file
-  deriving (Typeable)
-instance Eq AnyFile where
-  (==) a b =
-    case a of
-      AnyFile a' -> case fromAnyFile b of
-                      Just b' -> (==) a' b'
-                      Nothing -> False
-instance Ord AnyFile where
-  compare a b =
-    case a of
-      AnyFile a' -> case fromAnyFile b of
-                      Just b' -> compare a' b'
-                      Nothing -> on compare fileType a b
-instance File AnyFile where
-  fromAnyFile (AnyFile file) = cast file
-  fileType (AnyFile file) = fileType file
-  filePath (AnyFile file) = filePath file
-  fileProvenance (AnyFile file) = fileProvenance file
-instance TextShow AnyFile where
-  textShow (AnyFile file) = textShow file
-
-data Mode
-  = HelpMode
-  | TaskMode Task AnyTarget
-  deriving (Eq, Ord)
-
-data Task
-  = AmalgamationTask
-  | BinaryTask
-  | TestTask
-  | DebugTask
-  | CleanTask
-  deriving (Eq, Ord)
-
-data Project =
-  Project {
-      projectName :: Text.Text,
-      projectDefaultTarget :: Maybe AnyTarget,
-      projectTargets :: Set.Set AnyTarget
-    }
-instance Eq Project where
-  (==) a b = on (==) projectName a b
-instance Ord Project where
-  compare a b = on compare projectName a b
-instance Named Project where
-  namedName = projectName
-
 data ExecutableTarget =
   ExecutableTarget {
-      executableTargetName :: Text.Text,
-      executableTargetPrerequisites :: Set.Set AnyTarget,
-      executableTargetPrivateHeaders :: Set.Set HeaderFile,
-      executableTargetSources :: Set.Set SourceFile
+      _executableTargetName :: Text.Text,
+      _executableTargetPrerequisites :: Set.Set AnyTarget,
+      _executableTargetPrivateHeaders :: Set.Set HeaderFile,
+      _executableTargetSources :: Set.Set SourceFile
     }
-instance Named ExecutableTarget where
-  namedName = executableTargetName
+makeLenses ''ExecutableTarget
+instance HasName ExecutableTarget where
+  nameOf = executableTargetName
 instance Target ExecutableTarget where
   targetBuildSteps = computeExecutableBuildSteps
   targetPrerequisites = executableTargetPrerequisites
@@ -158,14 +322,15 @@ instance TextShow ExecutableTarget where
 
 data LibraryTarget =
   LibraryTarget {
-      libraryTargetName :: Text.Text,
-      libraryTargetPrerequisites :: Set.Set AnyTarget,
-      libraryTargetPublicHeaders :: Set.Set HeaderFile,
-      libraryTargetPrivateHeaders :: Set.Set HeaderFile,
-      libraryTargetSources :: Set.Set SourceFile
+      _libraryTargetName :: Text.Text,
+      _libraryTargetPrerequisites :: Set.Set AnyTarget,
+      _libraryTargetPublicHeaders :: Set.Set HeaderFile,
+      _libraryTargetPrivateHeaders :: Set.Set HeaderFile,
+      _libraryTargetSources :: Set.Set SourceFile
     }
-instance Named LibraryTarget where
-  namedName = libraryTargetName
+makeLenses ''LibraryTarget
+instance HasName LibraryTarget where
+  nameOf = libraryTargetName
 instance Target LibraryTarget where
   targetBuildSteps = computeLibraryBuildSteps
   targetPrerequisites = libraryTargetPrerequisites
@@ -199,178 +364,14 @@ instance TextShow LibraryTarget where
         $ libraryTargetSources target,
        "))"]
 
-data Provenance
-  = InputProvenance
-  | BuiltProvenance
-  | SystemProvenance
-  deriving (Eq, Ord)
-instance TextShow Provenance where
-  textShow InputProvenance = "input-provenance"
-  textShow BuiltProvenance = "build-provenance"
-  textShow SystemProvenance = "system-provenance"
-
-data HeaderFile =
-  HeaderFile {
-      headerFileLanguage :: Language,
-      headerFilePath :: Text.Text,
-      headerFileProvenance :: Provenance
-    }
-  deriving (Typeable)
-instance Eq HeaderFile where
-  (==) a b =
-    foldl1 (&&)
-           [on (==) headerFileLanguage a b,
-            on (==) headerFilePath a b,
-            on (==) headerFileProvenance a b]
-instance Ord HeaderFile where
-  compare a b =
-    mconcat [on compare headerFileLanguage a b,
-             on compare headerFilePath a b,
-             on compare headerFileProvenance a b]
-instance File HeaderFile where
-  fromAnyFile (AnyFile file) = cast file
-  fileType file = HeaderFileType $ headerFileLanguage file
-  filePath = headerFilePath
-  fileProvenance = headerFileProvenance
-instance TextShow HeaderFile where
-  textShow file =
-    Text.concat $
-      ["(header-file \"",
-       headerFilePath file,
-       "\" ",
-       textShow $ headerFileProvenance file,
-       " ",
-       textShow $ headerFileLanguage file,
-       ")"]
-
-data SourceFile =
-  SourceFile {
-      sourceFileLanguage :: Language,
-      sourceFilePath :: Text.Text,
-      sourceFileProvenance :: Provenance
-    }
-  deriving (Typeable)
-instance Eq SourceFile where
-  (==) a b =
-    foldl1 (&&)
-           [on (==) sourceFileLanguage a b,
-            on (==) sourceFilePath a b,
-            on (==) sourceFileProvenance a b]
-instance Ord SourceFile where
-  compare a b =
-    mconcat [on compare sourceFileLanguage a b,
-             on compare sourceFilePath a b,
-             on compare sourceFileProvenance a b]
-instance File SourceFile where
-  fromAnyFile (AnyFile file) = cast file
-  fileType file = SourceFileType $ sourceFileLanguage file
-  filePath = sourceFilePath
-  fileProvenance = sourceFileProvenance
-instance TextShow SourceFile where
-  textShow file =
-    Text.concat $
-      ["(source-file \"",
-       sourceFilePath file,
-       "\" ",
-       textShow $ sourceFileProvenance file,
-       " ",
-       textShow $ sourceFileLanguage file,
-       ")"]
-
-data ObjectFile =
-  ObjectFile {
-      objectFilePath :: Text.Text,
-      objectFileProvenance :: Provenance
-    }
-  deriving (Typeable)
-instance Eq ObjectFile where
-  (==) a b =
-    foldl1 (&&)
-           [on (==) objectFilePath a b,
-            on (==) objectFileProvenance a b]
-instance Ord ObjectFile where
-  compare a b =
-    mconcat [on compare objectFilePath a b,
-             on compare objectFileProvenance a b]
-instance File ObjectFile where
-  fromAnyFile (AnyFile file) = cast file
-  fileType _ = ObjectFileType
-  filePath = objectFilePath
-  fileProvenance = objectFileProvenance
-instance TextShow ObjectFile where
-  textShow file =
-    Text.concat $
-      ["(object-file \"",
-       objectFilePath file,
-       "\" ",
-       textShow $ objectFileProvenance file,
-       ")"]
-
-data ExecutableFile =
-  ExecutableFile {
-      executableFilePath :: Text.Text,
-      executableFileProvenance :: Provenance
-    }
-  deriving (Typeable)
-instance Eq ExecutableFile where
-  (==) a b =
-    foldl1 (&&)
-           [on (==) executableFilePath a b,
-            on (==) executableFileProvenance a b]
-instance Ord ExecutableFile where
-  compare a b =
-    mconcat [on compare executableFilePath a b,
-             on compare executableFileProvenance a b]
-instance File ExecutableFile where
-  fromAnyFile (AnyFile file) = cast file
-  fileType _ = ExecutableFileType
-  filePath = executableFilePath
-  fileProvenance = executableFileProvenance
-instance TextShow ExecutableFile where
-  textShow file =
-    Text.concat $
-      ["(executable-file \"",
-       executableFilePath file,
-       "\" ",
-       textShow $ executableFileProvenance file,
-       ")"]
-
-data LibraryFile =
-  LibraryFile {
-      libraryFilePath :: Text.Text,
-      libraryFileProvenance :: Provenance
-    }
-  deriving (Typeable)
-instance Eq LibraryFile where
-  (==) a b =
-    foldl1 (&&)
-           [on (==) libraryFilePath a b,
-            on (==) libraryFileProvenance a b]
-instance Ord LibraryFile where
-  compare a b =
-    mconcat [on compare libraryFilePath a b,
-             on compare libraryFileProvenance a b]
-instance File LibraryFile where
-  fromAnyFile (AnyFile file) = cast file
-  fileType _ = LibraryFileType
-  filePath = libraryFilePath
-  fileProvenance = libraryFileProvenance
-instance TextShow LibraryFile where
-  textShow file =
-    Text.concat $
-      ["(library-file \"",
-       libraryFilePath file,
-       "\" ",
-       textShow $ libraryFileProvenance file,
-       ")"]
-
 data Invocation =
   Invocation {
-      invocationExecutable :: ExecutableFile,
-      invocationParameters :: [Text.Text],
-      invocationInputs :: Set.Set AnyFile,
-      invocationOutputs :: Set.Set AnyFile
+      _invocationExecutable :: ExecutableFile,
+      _invocationParameters :: [Text.Text],
+      _invocationInputs :: Set.Set AnyFile,
+      _invocationOutputs :: Set.Set AnyFile
     }
+makeLenses ''Invocation
 instance BuildStep Invocation where
   buildStepInputs = invocationInputs
   buildStepOutputs = invocationOutputs
@@ -391,13 +392,24 @@ instance TextShow Invocation where
         $ invocationOutputs invocation,
        "))"]
 
-data Language
-  = CLanguage
-  | HaskellLanguage
+data Mode
+  = HelpMode
+  | TaskMode Task AnyTarget
   deriving (Eq, Ord)
-instance TextShow Language where
-  textShow CLanguage = "c-language"
-  textShow HaskellLanguage = "haskell-language"
+
+data Project =
+  Project {
+      _projectName :: Text.Text,
+      _projectDefaultTarget :: Maybe AnyTarget,
+      _projectTargets :: Set.Set AnyTarget
+    }
+makeLenses ''Project
+instance Eq Project where
+  (==) a b = on (==) _projectName a b
+instance Ord Project where
+  compare a b = on compare _projectName a b
+instance HasName Project where
+  name = projectName
 
 
 main :: IO ()
